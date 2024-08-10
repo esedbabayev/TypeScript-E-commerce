c50c03fe78586a33cc17880e54fd024c2eb9ec39
Asad Babayev <esedbabayev12@gmail.com>
2024-08-10 18:55:50 +0400

    adjust width

--- src/components/Products/LeftSidebarSection.tsx
+++ src/components/Products/LeftSidebarSection.tsx
@@ -8,7 +8,7 @@ import Price from "./Price.tsx";
 
 const LeftSidebarSection: React.FC = () => {
   return (
-    <aside className="w-[248px] h-3/4 flex flex-col gap-10 px-5 py-6 border rounded-md border-[#E6E7E8]">
+    <aside className="w-1/4 h-3/4 flex flex-col gap-10 px-5 py-6 border rounded-md border-[#E6E7E8]">
       <Categories />
       <Colors />
       <Sizes />
