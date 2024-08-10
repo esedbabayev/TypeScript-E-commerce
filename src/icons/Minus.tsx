997b310cc4c11882a682f9a51dc1ed274d3a0aeb
Asad Babayev <esedbabayev12@gmail.com>
2024-08-10 23:20:49 +0400

    add icons

---
+++ src/icons/Minus.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Minus: React.FC = () => {
+  return (
+    <svg
+      width="20"
+      height="20"
+      viewBox="0 0 20 20"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M4.16669 10H14.1667"
+        stroke="#5C5F6A"
+        stroke-width="1.7"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+};
+
+export default Minus;
