b89f54ebde10c351fb162cea69c29d40962a46c6
Asad Babayev <esedbabayev12@gmail.com>
2024-08-10 17:33:52 +0400

    add showing results and sort by

--- src/components/Products/ProductsGridSection.tsx
+++ src/components/Products/ProductsGridSection.tsx
@@ -1,35 +1,48 @@
-import React from 'react'
+import React from "react";
 
-const ProductsGridSection:React.FC = () => {
+// Components
+
+// Icons
+import ChevronDown from "../../icons/ChevronDown.tsx";
+
+const ProductsGridSection: React.FC = () => {
   return (
-    <div className='flex flex-col gap-10'>
+    <div className="w-full flex flex-col gap-10">
       {/* applied filters */}
-      <div className='flex flex-col gap-3'>
-        <p className='font-medium'>Applied Filters:</p>
+      <div className="flex flex-col gap-3">
+        <p className="font-medium">Applied Filters:</p>
         {/* filter div */}
-        <div className='flex gap-4'>
-          <div className='px-4 py-3 border rounded-3xl'>
-            <span className='flex gap-4'>Perfume<span className='cursor-pointer'>&#10005;</span></span>
+        <div className="flex gap-4">
+          <div className="px-4 py-3 border rounded-3xl">
+            <span className="flex gap-4">
+              Perfume<span className="cursor-pointer">&#10005;</span>
+            </span>
           </div>
-          <div className='px-4 py-3 border rounded-3xl'>
-            <span className='flex gap-4'>Size: M<span className='cursor-pointer'>&#10005;</span></span>
+          <div className="px-4 py-3 border rounded-3xl">
+            <span className="flex gap-4">
+              Size: M<span className="cursor-pointer">&#10005;</span>
+            </span>
           </div>
         </div>
       </div>
       {/* showing results */}
-      <div>
-
+      <div className="w-full flex justify-between">
+        <div>
+          <span className="text-[#5C5F6A] font-medium">Showing 1-9 of 36 results.</span>
+        </div>
+        <div className="flex gap-3 items-center cursor-pointer">
+          <span className="uppercase">Sort by</span>
+          <div>
+            <ChevronDown />
+          </div>
+        </div>
       </div>
       {/* proucts */}
-      <div>
-
-      </div>
+      <div></div>
       {/* pagination */}
-      <div>
-
-      </div>
+      <div></div>
     </div>
-  )
-}
+  );
+};
 
-export default ProductsGridSection
+export default ProductsGridSection;
