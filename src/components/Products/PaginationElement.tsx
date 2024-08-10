ff97417f47daf3301debd83233a1f2a384a67294
Asad Babayev <esedbabayev12@gmail.com>
2024-08-10 20:19:53 +0400

    add pagination element

---
+++ src/components/Products/PaginationElement.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const PaginationElement: React.FC = () => {
+  return (
+    <div className="px-4 py-1 bg-[#F6F6F6] rounded-md cursor-pointer">
+      <span>1</span>
+    </div>
+    // <div className="px-4 py-1 rounded-md cursor-pointer">
+    //   <span>1</span>
+    // </div>
+  );
+};
+
+export default PaginationElement;
