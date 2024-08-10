c4cc600ab21aa4fd8fe8d4cef347e0f2cd583343
Asad Babayev <esedbabayev12@gmail.com>
2024-08-10 17:12:28 +0400

    add applied filters

--- src/components/Products/ProductsGridSection.tsx
+++ src/components/Products/ProductsGridSection.tsx
@@ -2,8 +2,32 @@ import React from 'react'
 
 const ProductsGridSection:React.FC = () => {
   return (
-    <div>
-      
+    <div className='flex flex-col gap-10'>
+      {/* applied filters */}
+      <div className='flex flex-col gap-3'>
+        <p className='font-medium'>Applied Filters:</p>
+        {/* filter div */}
+        <div className='flex gap-4'>
+          <div className='px-4 py-3 border rounded-3xl'>
+            <span className='flex gap-4'>Perfume<span className='cursor-pointer'>&#10005;</span></span>
+          </div>
+          <div className='px-4 py-3 border rounded-3xl'>
+            <span className='flex gap-4'>Size: M<span className='cursor-pointer'>&#10005;</span></span>
+          </div>
+        </div>
+      </div>
+      {/* showing results */}
+      <div>
+
+      </div>
+      {/* proucts */}
+      <div>
+
+      </div>
+      {/* pagination */}
+      <div>
+
+      </div>
     </div>
   )
 }
