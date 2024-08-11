e36b00bd2d5e4c6bb9d27913c367cd9203c7c299
Asad Babayev <esedbabayev12@gmail.com>
2024-08-12 02:11:28 +0400

    create sign up section

---
+++ src/components/SignUp/SignUpSection.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+
+// Link
+import { Link } from "react-router-dom";
+
+// Components
+import Container from "../Container.tsx";
+import Google from "../Google.tsx";
+import Form from "../Form.tsx";
+import InputField from "../InputField.tsx";
+
+const SignUpSection: React.FC = () => {
+  return (
+    <section className="mt-20 mb-40">
+      <Container>
+        <div className="w-full justify-center items-center flex flex-col gap-10">
+          <Google />
+          <div className="w-full flex flex-col items-center justify-center gap-6">
+            <Form
+              inputElementName={<InputField label="name" />}
+              inputElementEmail={<InputField label="email" />}
+              inputElementPassword={<InputField label="password" />}
+            />
+            <div className="flex justify-end w-1/5">
+              <span className="text-xs font-medium text-[#474B57] cursor-pointer hover:underline">
+                By creating an account you agree with our Terms of Service,
+                Privacy Policy.
+              </span>
+            </div>
+            <div className="w-1/5">
+              <button className="w-full flex gap-3 items-center justify-center px-6 py-2 bg-black text-white rounded-md border-2 border-transparent transition-all duration-200 hover:bg-[#F6F6F6] hover:text-black hover:border-black">
+                Create account
+              </button>
+            </div>
+            <div>
+              <span className="text-xs font-medium text-[#474B57]">
+                Already have an account?{" "}
+                <Link className="hover:underline" to="/sign-in">
+                  {" "}
+                  Log in
+                </Link>
+              </span>
+            </div>
+          </div>
+        </div>
+      </Container>
+    </section>
+  );
+};
+
+export default SignUpSection;
