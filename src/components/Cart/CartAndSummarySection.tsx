95fd33098de45f7a95e0f720c5eb51f1cab4eb77
Asad Babayev <esedbabayev12@gmail.com>
2024-08-11 12:06:21 +0400

    create order summary component

--- src/components/Cart/CartAndSummarySection.tsx
+++ src/components/Cart/CartAndSummarySection.tsx
@@ -3,13 +3,15 @@ import React from "react";
 // Components
 import Container from "../Container.tsx";
 import Cart from "./Cart.tsx";
+import OrderSummary from "./OrderSummary.tsx";
 
 const CartAndSummarySection: React.FC = () => {
   return (
     <section className="mt-20">
       <Container>
-        <div>
+        <div className="w-full flex justify-between">
           <Cart />
+          <OrderSummary />
         </div>
       </Container>
     </section>
---
+++ src/components/Cart/OrderSummary.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const OrderSummary:React.FC = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default OrderSummary
