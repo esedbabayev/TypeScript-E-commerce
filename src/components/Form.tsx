489e297e1612401e4f444bf4f8dd4cdf6ed78e94
Asad Babayev <esedbabayev12@gmail.com>
2024-08-12 02:09:37 +0400

    add props types

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,16 +1,24 @@
 import React from "react";
 
-// Components
-import InputField from "./InputField.tsx";
+type Props = {
+  inputElementEmail: React.ReactNode;
+  inputElementPassword: React.ReactNode;
+  inputElementName?: React.ReactNode;
+};
 
-const Form: React.FC = () => {
+const Form: React.FC<Props> = ({
+  inputElementEmail,
+  inputElementPassword,
+  inputElementName,
+}) => {
   return (
     <form
       className="w-1/5 flex flex-col gap-4"
       onSubmit={(e) => e.preventDefault()}
     >
-      <InputField label="email" />
-      <InputField label="password" />
+      {inputElementName}
+      {inputElementEmail}
+      {inputElementPassword}
     </form>
   );
 };
