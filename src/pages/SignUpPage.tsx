fbb3cf51cb4e8099fbcd2e3bbc53e2b4eb1b8098
Asad Babayev <esedbabayev12@gmail.com>
2024-08-12 02:11:43 +0400

    complete sign up page

--- src/pages/SignUpPage.tsx
+++ src/pages/SignUpPage.tsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from "react";
+
+// Components
+import TopEcommerceSection from "../components/TopEcommerceSection.tsx";
+import SignUpSection from "../components/SignUp/SignUpSection.tsx";
 
 const SignUpPage: React.FC = () => {
   return (
-    <div>
+    <main>
+      <TopEcommerceSection nameOfPage="Sign up" search="Sign up" />
+      <SignUpSection />
+    </main>
+  );
+};
 
-    </div>
-  )
-}
-
-export default SignUpPage
+export default SignUpPage;
