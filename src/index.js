55905b357305936a94ca1c89d5c7858269cc490c
Asad Babayev <esedbabayev12@gmail.com>
2024-08-16 01:23:10 +0400

    import Provider & store

--- src/index.js
+++ src/index.js
@@ -6,9 +6,17 @@ import App from "./App.tsx";
 // BrowserRouter
 import { BrowserRouter as Router } from "react-router-dom";
 
+// Provider
+import { Provider } from "react-redux";
+
+// Store
+import store from "./store.js"
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Router>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </Router>
 );
