8bd5f4da4e2df8aa790fbf32d91c7b6a598ec752
asadbabayev <asadyb@code.edu.az>
2024-08-16 18:19:40 +0400

    fetch data

--- src/components/Product/ProductSection.tsx
+++ src/components/Product/ProductSection.tsx
@@ -1,16 +1,39 @@
 import React from "react";
 
+// Hooks
+import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+
 // Components
 import Container from "../Container.tsx";
 import ProductImage from "./ProductImage.tsx";
 import ProductInfo from "./ProductInfo.tsx";
+import ProductsGridSection from "../Products/ProductsGridSection";
 
 const ProductSection: React.FC = () => {
+  const { id } = useParams();
+
+  const [productData, setProductData] = useState();
+
+  const getProductById = async () => {
+    const response = await fetch(`http:localhost:3000/users?id=${id}`);
+    const data = await response.json();
+    setProductData(data);
+  };
+
+  useEffect(() => {
+    getProductById();
+  }, [id]);
+
   return (
     <section className="mt-4">
       <Container>
         <div className="w-full flex gap-8">
-          <ProductImage />
+          {
+            productData.map(() => (
+              <ProductImage image={}/>
+            ))
+          }
           <ProductInfo />
         </div>
       </Container>
