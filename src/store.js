86c67778a4826d2f78bde74069bf038f441fc64c
Asad Babayev <esedbabayev12@gmail.com>
2024-08-18 02:11:51 +0400

    import slices & add reducers

--- src/store.js
+++ src/store.js
@@ -1,9 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
 
 // Slices
+import categorySlice from "./Slices/categories.slice";
+import colorSlice from "./Slices/colors.slice";
+import sizeSlice from "./Slices/sizes.slice";
 
 const store = configureStore({
-  reducer: {},
+  reducer: {
+    categories: categorySlice.reducer,
+    colors: colorSlice.reducer,
+    sizes: sizeSlice.reducer,
+  },
 });
 
 export default store;
