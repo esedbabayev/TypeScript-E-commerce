847cad012e7016e2b90375458bc671a4adef7176
Asad Babayev <esedbabayev12@gmail.com>
2024-08-19 01:23:09 +0400

    add getSpecificCart

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,16 @@
 import React from "react";
 
 // React Router Dom
-import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+
+// Hooks
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+
+// Actions
+import { setCartItems } from "./Slices/cart.slice.js";
 
 // Components
-import Container from "./components/Container.tsx";
 import Layout from "./components/layout/Layout.tsx";
 
 // Pages
@@ -17,6 +23,25 @@ import SignUpPage from "./pages/SignUpPage.tsx";
 import CartPage from "./pages/CartPage.tsx";
 
 const App: React.FC = () => {
+  // const dispatch = useDispatch();
+
+  // const userId = JSON.parse(localStorage.getItem("userId"));
+
+  // const getSpecificCart = async () => {
+  //   const response = await fetch(
+  //     `http://localhost:3000/carts?userId=${userId}`
+  //   );
+  //   const data = response.json();
+  //   console.log(data);
+    
+
+  //   dispatch(setCartItems(data));
+  // };
+
+  // useEffect(() => {
+  //   getSpecificCart();
+  // }, [userId]);
+
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
