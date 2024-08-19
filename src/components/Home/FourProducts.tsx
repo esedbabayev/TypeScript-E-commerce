763789714b61ab0f2ae31166f6a172be167ecf79
asadbabayev <asadyb@code.edu.az>
2024-08-19 14:04:09 +0400

    fetch data for fourProducts

--- src/components/Home/FourProducts.tsx
+++ src/components/Home/FourProducts.tsx
@@ -1,18 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 
 // Components
-import ProductItem from '../ProductItem.tsx'
+import ProductItem from "../ProductItem.tsx";
 
 const FourProducts: React.FC = () => {
-  return (
-    <div className='grid grid-cols-4 gap-7'>
-      {/* Product */}
-      <ProductItem />
-      <ProductItem />
-      <ProductItem />
-      <ProductItem />
-    </div>
-  )
-}
+  const [products, setProducts] = useState();
 
-export default FourProducts
+  const fetchProducts = async () => {
+    const response = await fetch("http://localhost:3000/products");
+    const data = await response.json();
+    setProducts(data);
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+  return (
+    <div className="grid grid-cols-4 gap-7">
+      {/* Product */}
+      {products?.map((product) => {
+       return <ProductItem product={product} />;
+      }).splice(0, 4)}
+    </div>
+  );
+};
+
+export default FourProducts;
